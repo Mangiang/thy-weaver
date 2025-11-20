7bd1ccbee09b0852d610ed27226a0b10c552a7fa
greatsquare0 <greatsquare0@proton.me>
2025-11-19 23:56:28 -0300

    [plugin-executable] Check if using Bun

--- packages/plugin-executable/src/index.ts
+++ packages/plugin-executable/src/index.ts
@@ -1,14 +1,21 @@
-import type { Plugin } from "@thy-weaver/core/plugin_helpers";
+import { fancyLogFormater, type Plugin } from "@thy-weaver/core/plugin_helpers";
 import { Command } from "commander";
 
-const manifest = (options?: any): Plugin => {
-  console.log(options);
+const manifest = (): Plugin => {
   return {
     name: "executable-maker",
     command: new Command("buildExec")
       .description("Compiles and creates a standalone executable for the game")
       .action(() => {
-        console.log("Hello");
+        if (process.versions.bun) {
+          console.log("Hello");
+        } else {
+          console.log(
+            fancyLogFormater("BUNDLER", "ERROR", {
+              message: "Bun is required to make executables",
+            }),
+          );
+        }
       }),
   };
 };
